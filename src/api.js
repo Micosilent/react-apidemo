fa7835fca8657602d13d00056998d90b80a6e977
Pau Costa <pau@micodev.es>
2021-08-30 18:39:17 +0200

    initial api config

---
+++ src/api.js
@@ -0,0 +1,35 @@
+import { config } from "./config";
+
+//Base API URL
+const base_url = "https://api.rawg.io/api/";
+//API Key
+const apiKey = config.APIKEY;
+const currentYear = new Date().getFullYear();
+
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  //Adding 0 padding for API request
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+const currentMonth = getCurrentMonth();
+
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+const popular_games = `games?key=${apiKey}dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const getPopularGamesURL = () => `${base_url}${popular_games}`;
