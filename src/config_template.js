254e22b51c12c2711ba0f9b87b3f3ccd39433743
Pau Costa <mico@micodev.es>
2021-10-04 11:36:47 +0200

    Refactored secret storage

---
+++ src/config_template.js
@@ -0,0 +1,3 @@
+/**This is a template to store the API key from rawg.io, you can get yours from https://rawg.io/apidocs,
+ *  replace it in this document, and rename it to config.js */
+export let config = { APIKEY: "REPLACE_WITH_YOUR_APIKEY" };
