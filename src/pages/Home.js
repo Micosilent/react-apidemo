36b46d91dbfcafdb4e7f1190c10158f77386ba24
Pau Costa <mico@micodev.es>
2021-10-04 12:45:26 +0200

    Added styling to game cards and home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,50 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesActions";
+//Components
+import Game from "../components/Game";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   //Fetch Games
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  //Store the data in a variable
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
 
   return (
-    <div className="HomePage">
-      <h1>Hello Ignite</h1>
-    </div>
+    <StyledGameList>
+      <h2>Upcoming Games</h2>
+      <StyledGames>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </StyledGames>
+    </StyledGameList>
   );
 };
 
+const StyledGameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const StyledGames = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 export default Home;
