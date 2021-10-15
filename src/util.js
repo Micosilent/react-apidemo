1cc584e7f3ec5c0f6907361058a46c2250327c6b
Pau Costa <mico@micodev.es>
2021-10-15 14:50:22 +0200

    Added image for null imagePath return from api

--- src/util.js
+++ src/util.js
@@ -1,6 +1,10 @@
 //Media Resize
 
-export const smallImage = (imagePath, size) => {
+export const smallImage = (imagePath, size, id) => {
+  if (imagePath === null) {
+    imagePath =
+      "https://media.rawg.io/media/screenshots/18f/18fe55fc0476e24d5a1ebdba6b6b4e1a.jpg";
+  }
   const image = imagePath.match(/media\/screenshots/)
     ? imagePath.replace(
         "media/screenshots",
